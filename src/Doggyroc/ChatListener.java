7ca537388afc935f3e5a35d06b0d46a4ff755393
doggyroc <doggyroc@gmail.com>
2012-08-18 23:03:39 -0400

    Created Config

--- src/Doggyroc/ChatListener.java
+++ src/Doggyroc/ChatListener.java
@@ -1,7 +1,5 @@
 package Doggyroc;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -10,30 +8,34 @@ import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class ChatListener implements Listener {
 	
+	Prefixed plugin;
+	
+	public ChatListener(Prefixed instance){
+		plugin = instance;
+	}
+	
 	@EventHandler
 	public void onPlayerChat(final AsyncPlayerChatEvent e){
 		if(e.isCancelled()) return;
 		
-//Strings&String Checks
-		
+		String message;
 		String world = e.getPlayer().getWorld().getName();
-		String message = e.getMessage().replaceAll("%", "%%");
-		Player player = e.getPlayer();
-		String sender = e.getPlayer().getDisplayName();
-		String prefix = PermissionsEx.getPermissionManager().getUser(player).getPrefix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
-		String suffix = PermissionsEx.getPermissionManager().getUser(player).getSuffix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
+		String player = e.getPlayer().getDisplayName();
+		String prefix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getPrefix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
+		String suffix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getSuffix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
 		
-//if Statements
+//If Statements
 		
 		if(prefix == null) prefix = "";
 		if(suffix == null) suffix = "";
 		if(PermissionsEx.getPermissionManager().getUser(e.getPlayer()).has("Prefixed.colour")){
 			message = e.getMessage().replaceAll("%", "%%").replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
+		}else {
+			message = e.getMessage().replaceAll("%", "%%");
 		}
-			
-//Actual Reformatting
 		
-		e.setFormat(prefix + ChatColor.WHITE + sender + suffix + ChatColor.WHITE + ": " + message);
+//Formatting
+		
+		e.setFormat(plugin.getConfig().getString("format").replaceAll("-Prefix", prefix).replaceAll("-Suffix", suffix).replaceAll("-Player", player).replaceAll("-Message", message).replaceAll("%", "%%").replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2"));
 	}
-		
 }
