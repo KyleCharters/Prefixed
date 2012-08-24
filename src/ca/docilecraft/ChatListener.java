fd8717746a993a4b6badda201cbe5476f1cf4b6e
doggyroc <doggyroc@gmail.com>
2012-08-24 13:04:04 -0400

    Integrating Vault!

--- src/ca/docilecraft/ChatListener.java
+++ src/ca/docilecraft/ChatListener.java
@@ -1,5 +1,6 @@
 package ca.docilecraft;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -21,21 +22,63 @@ public class ChatListener implements Listener {
 		String message;
 		String world = e.getPlayer().getWorld().getName();
 		String player = e.getPlayer().getDisplayName();
-		String prefix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getPrefix(world);
-		String suffix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getSuffix(world);
-		
+
 //If Statements
 		
-		if(prefix == null) prefix = "";
-		if(suffix == null) suffix = "";
-		if(PermissionsEx.getPermissionManager().getUser(e.getPlayer()).has("Prefixed.colour")){
-			message = e.getMessage().replaceAll("%", "%%").replace("&", "§");
+		if(permission(e.getPlayer(), "Prefixed.colour")){
+			message = e.getMessage().replace("%", "%%").replace("&", "§");
 		}else {
 			message = e.getMessage().replace("%", "%%");
 		}
 		
 //Formatting
 		
-		e.setFormat(plugin.getConfig().getString("format").replace("-Prefix", prefix).replace("-Suffix", suffix).replace("-Player", player).replace("&", "§").replace("-Message", message));
+		e.setFormat(plugin.getConfig().getString("format").replace("-Prefix", prefix(e.getPlayer().getName(), world)).replace("-Suffix", suffix(e.getPlayer().getName(), world)).replace("-Player", player).replace("&", "§").replace("-Message", message));
+	}
+	
+	
+	
+	
+	public String prefix(String player, String world){
+		if(plugin.getServer().getPluginManager().isPluginEnabled("Vault")){
+			String prefix = Prefixed.chat.getPlayerPrefix(world, player);
+			if(prefix == null) prefix = "";
+			return prefix;
+		}else if(plugin.getServer().getPluginManager().isPluginEnabled("PermissionsEx")){
+			String prefix = PermissionsEx.getPermissionManager().getUser(player).getPrefix(world);
+			if(prefix == null) prefix = "";
+			return prefix;
+		}
+		return "";
+	}
+	
+	
+	
+	
+	public String suffix(String player, String world){
+		if(plugin.getServer().getPluginManager().isPluginEnabled("Vault")){
+			String suffix = Prefixed.chat.getPlayerSuffix(world, player);
+			if(suffix == null) suffix = "";
+			return suffix;
+		}else if(plugin.getServer().getPluginManager().isPluginEnabled("PermissionsEx")){
+			String suffix = PermissionsEx.getPermissionManager().getUser(player).getSuffix(world);
+			if(suffix == null) suffix = "";
+			return suffix;
+		}
+		return "";
+	}
+	
+	
+	
+	
+	public boolean permission(Player player, String permission){
+		if(plugin.getServer().getPluginManager().isPluginEnabled("Vault")){
+			boolean haspermission = Prefixed.permission.has(player, permission);
+			return haspermission;
+		}else if(plugin.getServer().getPluginManager().isPluginEnabled("PermissionsEx")){
+			boolean haspermission = PermissionsEx.getPermissionManager().getUser(player).has(permission);
+			return haspermission;
+		}
+		return false;
 	}
 }
