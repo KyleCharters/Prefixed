e1f0b06d50525f8b8d71df0e2ed117599fcba6bd
doggyroc <doggyroc@gmail.com>
2012-08-23 22:28:43 -0400

    Fixed $ and \ Giving errors.

--- src/ca/docilecraft/ChatListener.java
+++ src/ca/docilecraft/ChatListener.java
@@ -21,21 +21,21 @@ public class ChatListener implements Listener {
 		String message;
 		String world = e.getPlayer().getWorld().getName();
 		String player = e.getPlayer().getDisplayName();
-		String prefix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getPrefix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
-		String suffix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getSuffix(world).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
+		String prefix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getPrefix(world);
+		String suffix = PermissionsEx.getPermissionManager().getUser(e.getPlayer()).getSuffix(world);
 		
 //If Statements
 		
 		if(prefix == null) prefix = "";
 		if(suffix == null) suffix = "";
 		if(PermissionsEx.getPermissionManager().getUser(e.getPlayer()).has("Prefixed.colour")){
-			message = e.getMessage().replaceAll("%", "%%").replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2");
+			message = e.getMessage().replaceAll("%", "%%").replace("&", "§");
 		}else {
-			message = e.getMessage().replaceAll("%", "%%");
+			message = e.getMessage().replace("%", "%%");
 		}
 		
 //Formatting
 		
-		e.setFormat(plugin.getConfig().getString("format").replaceAll("-Prefix", prefix).replaceAll("-Suffix", suffix).replaceAll("-Player", player).replaceAll("(&([a-fl-o0-9A-FL-O]))", "\247$2").replaceAll("-Message", message));
+		e.setFormat(plugin.getConfig().getString("format").replace("-Prefix", prefix).replace("-Suffix", suffix).replace("-Player", player).replace("&", "§").replace("-Message", message));
 	}
 }
